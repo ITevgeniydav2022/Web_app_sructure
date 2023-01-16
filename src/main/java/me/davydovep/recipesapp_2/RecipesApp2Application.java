0121000d50a8e06b1899757736342d32e461eca2
ITevgeniydav2022 <defox2022@gmail.com>
2023-01-16 12:51:58 +0300

    Первый коммит

---
+++ src/main/java/me/davydovep/recipesapp_2/RecipesApp2Application.java
@@ -0,0 +1,13 @@
+package me.davydovep.recipesapp_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesApp2Application.class, args);
+    }
+
+}
